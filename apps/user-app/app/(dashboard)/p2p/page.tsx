bbdd37083065cf804c1ce1d73e036f02cbbb0509
prashantforsure <156293330+prashantforsure@users.noreply.github.com>
2024-04-09 21:48:59 +0530

    first commit

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,12 @@
+import { SendCard } from "../../../components/SendCard";
+
+export default function(){
+    return (
+        <>
+    <div className="w-full"> 
+<SendCard />
+    </div>
+    </>
+    )
+    
+}
